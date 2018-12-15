3a325e2553344639cbb6094366359f4240615cf1
Mykhailo Zabarin <m.zabarin@selectoglobal.com>
2018-12-15 03:18:28 +0200

    Extensions for Coding and Decodable, ImageItem Modelw were added

---
+++ MVVMTest/Utils/Extensions/ExtDecodable.swift
@@ -0,0 +1,9 @@
+//
+//  ExtDecodable.swift
+//  MVVMTest
+//
+//  Created by Mykhailo Zabarin on 12/15/18.
+//  Copyright © 2018 Mykhailo Zabarin. All rights reserved.
+//
+
+import Foundation
---
+++ MVVMTest/Utils/Types.swift
@@ -0,0 +1,9 @@
+//
+//  Types.swift
+//  MVVMTest
+//
+//  Created by Mykhailo Zabarin on 12/15/18.
+//  Copyright © 2018 Mykhailo Zabarin. All rights reserved.
+//
+
+import Foundation
